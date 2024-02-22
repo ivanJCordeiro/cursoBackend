bca2c926dc51f5a501684e02d284ea1fa4ec7696
ivan cordeiro <ivanjcordeiro94@gmail.com>
2024-02-22 13:21:35 -0300

    segunda actividad error en codeExists

--- work/primerDesafio.js
+++ work/primerDesafio.js
@@ -1,24 +1,29 @@
+const fs = require("fs");
+
 class ProductsManager{
 
     constructor (){
-        this.products = []
+        this.path = "./work/products.json"
+        try {
+            const data = fs.readFileSync(this.path, 'utf-8');
+            this.products = JSON.parse(data);
+        } catch (error) {
+            this.products = [];
+        }
         this.id = 1
     }
 
-    addProduct( title , description , price , thumbnail , code , stock ){
 
-        if (!title || !description || !price || !thumbnail || !code || !stock ){
-            console.warn("Todos los campos son obligatorios");
-        }
+    async addProduct( title , description , price , thumbnail , code , stock ){
 
-        const existCode = 
-            this.products.some(product =>
-                product.code === code)
+        // const existCode = 
+        //     this.products.some(product =>
+        //         product.code === code)
 
-        if (existCode){
-            console.error("Codigo en uso, por favor escribir otro");
-            return
-        }
+        // if (existCode){
+        //     console.error("Codigo en uso, por favor escribir otro");
+        //     return
+        // }
 
         const addNewProduct = {
             id : this.id ++,
@@ -32,45 +37,95 @@ class ProductsManager{
         
         this.products.push (addNewProduct)
         console.log("Producto agregado : ", addNewProduct);
+
+        await fs.promises.writeFile(this.path , JSON.stringify( this.products , null,"\t" ));
     }
 
-    getProducts () {
-        return this.products;
-    }
-
-    getProductById (id) {
-        if(this.products.some(product => product.id === id)){
-            return this.products.find(product => product.id === id);
-        } else {
-            console.error("Not found");
+    async getProducts () {
+        try {
+        const readFile = await fs.promises.readFile(this.path , "utf-8");
+        const productsObj = JSON.parse(readFile)
+        return productsObj;
+        }catch (error){
+            console.error("Error al obtener productos" , error);
+            return;
         }
+    }
+
+    async getProductById (id) {
+        try {
+            const productId = parseInt(id); 
+            const product = this.products.find(product => product.id === productId);
+            if (product) {
+                return product;
+            } else {
+                console.error("Producto no encontrado");
+                return null;
+            }
+        } catch (error) {
+            console.error("Error al obtener producto por ID", error);
+            return null;
+        }}
+
+    async updateProduct (id, data){
+        const findId = this.products.findIndex(product => product.id === id);
+        if (findId === -1) return console.error("Not found");
+
+        const updateProduct = {
+                ...this.products[findId],
+                ...data,
+                id : this.products[findId].id
+        };
+
+        this.products[findId] = updateProduct
+
+        await fs.promises.writeFile(this.path , JSON.stringify(this.products , null , "\t"));
+        console.log("Producto actualizado : " , updateProduct);
+    }
+
+    async deleteProduct (id){
+
+        const deleteForId = this.products.findIndex(product => product.id === id)
+        if(deleteForId === -1) {
+            console.error("Producto no encontrado");
+            return
+        };
+        this.products.splice(deleteForId , 1)
+
+        await fs.promises.writeFile(this.path , JSON.stringify(this.products, null, "\t"));
+        console.log(`Producto ${id} eliminado correctamente`);
     }
 }
 
 const products = new ProductsManager();
 
-products.addProduct(
-    "PALO VLACK EMULI PRO PLATA 22/33",
-    "Este palo fue desarrollado con el objetivo de perfeccionar la arrastrada. Su tres características principales harán que este gesto técnico alcance su máximo nivel: Canaleta a los 200mm, curvatura  y mango oval anatómico. Es uno de los palos más rígidos de la línea gracias a su 95% de carbono y fue diseñado para jugadores profesionales y/o experimentados.",
-    "$333.500",
-    "https://www.prohockeyshop.com.ar/productos/palo-vlack-emuli-pro-plata-22-33/?pf=gs&variant=723924740&gad_source=1&gclid=CjwKCAiArLyuBhA7EiwA-qo80P6SX7Zo_7p2oxvnNkeEsg3bzhDvE4bvF4pH4xiRBOWmtKAykO_JcRoCNzMQAvD_BwE#product-gallery-1",
-    "product_001",
-    "15"
-);
+async function addProductDetails(){
+    
+   await products.addProduct(
+        "PALO VLACK EMULI PRO PLATA 22/33",
+        "Este palo fue desarrollado con el objetivo de perfeccionar la arrastrada. Su tres características principales harán que este gesto técnico alcance su máximo nivel: Canaleta a los 200mm, curvatura  y mango oval anatómico. Es uno de los palos más rígidos de la línea gracias a su 95% de carbono y fue diseñado para jugadores profesionales y/o experimentados.",
+        "$333.500",
+        "sin imagen",
+        "product_0011",
+        "15"
+    );
+    
+    await   products.addProduct(
+        "Palo Hockey Grays Ac5 Dynabow Micro Carbono 60% Color Negro",
+        "El Palo de Hockey Grays AC5 Dynabow es una herramienta de alta calidad diseñada para jugadores senior que buscan mejorar su juego. Este palo, de color negro, es un producto de la reconocida marca Grays, conocida por su compromiso con la excelencia y la innovación en el mundo del hockey..",
+        "281.600",
+        "sin imagen",
+        "product_02",
+        "21"
+    )
 
-products.addProduct(
-    "Palo Hockey Grays Ac5 Dynabow Micro Carbono 60% Color Negro",
-    "El Palo de Hockey Grays AC5 Dynabow es una herramienta de alta calidad diseñada para jugadores senior que buscan mejorar su juego. Este palo, de color negro, es un producto de la reconocida marca Grays, conocida por su compromiso con la excelencia y la innovación en el mundo del hockey. El AC5 Dynabow está compuesto por un 60% de carbono, lo que le proporciona una resistencia y durabilidad excepcionales, permitiendo a los jugadores realizar golpes potentes y precisos. Este palo no es solo para jugadores de campo, sino que también es ideal para arqueros, gracias a su diseño y construcción. El material compuesto del palo asegura un rendimiento óptimo en cualquier tipo de campo, pero especialmente sobre hierba. Además, este modelo AC5 Dynabow, de origen asiático, cuenta con una curva Dynabow, que potencia el desarrollo de las destrezas y habilidades del jugador. En resumen, este palo de hockey Grays AC5 Dynabow es una inversión inteligente para cualquier jugador serio de hockey que busque mejorar su juego y rendimiento.",
-    "281.600",
-    "https://www.mercadolibre.com.ar/palo-hockey-grays-ac5-dynabow-micro-carbono-60-color-negro/p/MLA28732316?item_id=MLA1615378928&from=gshop&matt_tool=60803023&matt_word=&matt_source=google&matt_campaign_id=14242903088&matt_ad_group_id=125225550909&matt_match_type=&matt_network=g&matt_device=c&matt_creative=538641359510&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=735113679&matt_product_id=MLA28732316-product&matt_product_partition_id=2267978675651&matt_target_id=aud-2014906607167:pla-2267978675651&cq_src=google_ads&cq_cmp=14242903088&cq_net=g&cq_plt=gp&cq_med=pla&gad_source=1&gclid=CjwKCAiArLyuBhA7EiwA-qo80MISewjZmbrPx7Vt2Z80qD90VxdAYQLbIwrPZzlQyTEBADmw51VOKhoCePkQAvD_BwE#&gid=1&pid=1",
-    "product_002",
-    "21"
-)
+}
 
-console.log(products.getProducts());
+async function executeOperations() {
+    await addProductDetails();
+    console.log(await products.getProducts());
+    console.log(await products.getProductById(1));
+    console.log(await products.getProductById(2));
+}
 
-console.log(products.getProductById(1));
-
-console.log(products.getProductById(1));
-
-console.log(products.getProductById(30));
+executeOperations();
